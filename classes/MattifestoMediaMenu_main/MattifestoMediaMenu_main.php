3c3f352186380130ba18697adedb21f1521dd777
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2021-11-02 18:14:15 -0700

    Set as the default main menu in MattifestoMediaMenu_main.php

--- classes/MattifestoMediaMenu_main/MattifestoMediaMenu_main.php
+++ classes/MattifestoMediaMenu_main/MattifestoMediaMenu_main.php
@@ -1,37 +1,69 @@
 <?php
 
-final class MattifestoMediaMenu_main {
+final class
+MattifestoMediaMenu_main {
+
+    /* -- CBInstall interfaces -- */
+
+
 
     /**
      * @return void
      */
-    static function CBInstall_install(): void {
+    static function
+    CBInstall_install(
+    ): void {
+        $menuModelCBID = MattifestoMediaMenu_main::ID();
+
         $updater = CBModelUpdater::fetch(
             (object)[
                 'className' => 'CBMenu',
-                'ID' => MattifestoMediaMenu_main::ID(),
+                'ID' => $menuModelCBID,
                 'title' => 'Website',
                 'titleURI' => '/',
             ]
         );
 
-        CBModelUpdater::save($updater);
+        CBModelUpdater::save(
+            $updater
+        );
+
+        CB_StandardPageFrame::setDefaultMainMenuModelCBID(
+            $menuModelCBID
+        );
     }
+    /* CBInstall_install() */
+
+
 
     /**
      * @return [string]
      */
-    static function CBInstall_requiredClassNames(): array {
+    static function
+    CBInstall_requiredClassNames(
+    ): array {
         return [
+            'CB_StandardPageFrame',
             'CBMenu',
             'CBModelUpdater',
         ];
     }
+    /* CBInstall_requiredClassNames() */
+
+
+
+    /* -- functions -- */
+
+
 
     /**
      * @return ID
      */
-    static function ID(): string {
+    static function
+    ID(
+    ): string {
         return 'aaba3db523731306b216c46d9eab9770c0030bc6';
     }
+    /* ID() */
+
 }
